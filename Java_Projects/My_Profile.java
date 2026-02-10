95dd4cd342284a96e645781ee25ed5b6255cc168
Ntando Jacob <ntandojacob287@gmail.com>
2026-02-10 09:48:20 +0200

    Add My_Profile.java
    Basic "My Profile" using Java

---
+++ Java_Projects/My_Profile.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Ntando Jacob";
+        int age = 22;
+        double height = 1.87;
+        boolean student = false;
+        int numYearsLearning = 2;
+        char unit = 'm';
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("          My Profile                  ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        System.out.println("Name + Surname: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " " + unit);
+        System.out.println("Student: " + student);
+        System.out.println("Years learning Tech: " + numYearsLearning + " years");
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        System.out.print(name + " here, I'm " + age + " years old and I love Java.");
+    }
+}
